9a40ebf87a5eff2650c7889a562a9ebb47e3d804
Mike Rogne <mike@infinitywebcreations.com>
2017-11-21 16:23:00 -0600

    Update task_list.go - save to file, load from file (not working)

--- tasks/task_list.go
+++ tasks/task_list.go
@@ -1,7 +1,47 @@
 package tasks
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 // TaskList is a slice of Tasks
 type TaskList []Task
+
+// LoadFromFile loads a TaskList from file
+func LoadFromFile(filename string) (TaskList, error) {
+	bs, err := ioutil.ReadFile(filename)
+	tasks := new(TaskList)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bs, tasks)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return *tasks, nil
+}
+
+// SaveToFile accepts a TaskList reference and writes the serialized version to file.
+func SaveToFile(tasks *TaskList, filename string) error {
+	bs, err := json.Marshal(*tasks)
+
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filename, bs, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Add a Task to the list
 func (tl *TaskList) Add(t Task) {

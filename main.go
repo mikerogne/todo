34a95b3dab081c7c71077fef70857eaa584eed9a
Mike Rogne <mike@infinitywebcreations.com>
2017-11-21 23:00:11 -0600

    WIP

--- main.go
+++ main.go
@@ -2,23 +2,77 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"todo/tasks"
 )
 
 func main() {
+	jsonFilename := "tasks.json"
+	taskList, err := tasks.LoadFromFile(jsonFilename)
+
+	if err != nil {
+		fmt.Printf("Error loading %s: %s", jsonFilename, err)
+		os.Exit(1)
+	}
+
+	for {
+		input := ""
+		second := ""
+
+		fmt.Printf("> ")
+		numInputs, _ := fmt.Scanf("%s %s", &input, &second)
+
+		switch strings.ToLower(input) {
+		case "help":
+			fmt.Print(`Help:
+help		display this menu
+list		list all tasks (all, incomplete, complete)
+add			add new task
+del <id>	delete a task (specify last 5+ characters)
+com <id>	mark a task complete (specify last 5+ characters)
+inc <id>	mark a task incomplete (specify last 5+ characters)
+`)
+		case "list":
+			// todo
+		case "add":
+			var newTask tasks.Task
+			if newTask, err = getNewTask(); err != nil {
+				fmt.Println("Error getting task:", err)
+				os.Exit(1)
+			}
+
+			taskList.Add(newTask)
+
+			// Write to file
+			tasks.SaveToFile(&taskList, jsonFilename)
+			fmt.Println("Task saved!")
+
+		case "del":
+			// todo
+		case "com":
+			// todo
+		case "inc":
+			// todo
+		}
+
+	}
+}
+
+func getNewTask() (tasks.Task, error) {
 	scanner := bufio.NewScanner(os.Stdin)
-	taskList := new(tasks.TaskList)
 	taskName := ""
 
 	fmt.Printf("Task name: ")
 	scanner.Scan()
 	taskName = scanner.Text()
+	newTask := tasks.NewTask(taskName)
 
-	taskList.Add(tasks.NewTask(taskName))
+	if len(strings.TrimSpace(taskName)) == 0 {
+		return newTask, errors.New("You cannot enter a blank task")
+	}
 
-	tasks.SaveToFile(taskList, "tasks.json")
-
-	fmt.Printf("%+v", taskList)
+	return newTask, nil
 }

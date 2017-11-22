7317acc97ecd3133d496337eb6285a6964772e23
Mike Rogne <mike@infinitywebcreations.com>
2017-11-21 23:27:12 -0600

    WIP

--- main.go
+++ main.go
@@ -11,47 +11,64 @@ import (
 
 func main() {
 	jsonFilename := "tasks.json"
-	taskList, err := tasks.LoadFromFile(jsonFilename)
+	var taskList tasks.TaskList
 
-	if err != nil {
-		fmt.Printf("Error loading %s: %s", jsonFilename, err)
-		os.Exit(1)
+	if _, err := os.Stat(jsonFilename); !os.IsNotExist(err) {
+		taskList, err = tasks.LoadFromFile(jsonFilename)
+
+		if err != nil {
+			fmt.Printf("Error loading %s: %s", jsonFilename, err)
+			os.Exit(1)
+		}
+	} else {
+		taskList = tasks.TaskList{}
 	}
+
+	displayHelp("Welcome!")
 
 	for {
 		input := ""
 		second := ""
 
-		displayHelp("Welcome!")
 		fmt.Printf("Command> ")
 		fmt.Scanf("%s %s", &input, &second)
 
 		switch strings.ToLower(input) {
 		case "help":
 			displayHelp("Help:")
+
 		case "list":
-			// todo
+			displayTasks(&taskList)
+
 		case "add":
-			var newTask tasks.Task
-			if newTask, err = getNewTask(); err != nil {
+			newTask, err := getNewTask()
+			if err != nil {
 				fmt.Println("Error getting task:", err)
-				os.Exit(1)
+				continue
 			}
 
 			taskList.Add(newTask)
+			err = tasks.SaveToFile(&taskList, jsonFilename)
 
-			// Write to file
-			tasks.SaveToFile(&taskList, jsonFilename)
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				os.Exit(1)
+			}
+
 			fmt.Println("Task saved!")
 
 		case "del":
 			// todo
+
 		case "com":
 			// todo
+
 		case "inc":
 			// todo
+
 		case "quit":
 			os.Exit(0)
+
 		case "q":
 			os.Exit(0)
 		}
@@ -80,10 +97,24 @@ func displayHelp(title string) {
 help		display this menu
 list		list all tasks (all, incomplete, complete)
 add		add new task
-del <id>	delete a task (specify last 5+ characters)
-com <id>	mark a task complete (specify last 5+ characters)
-inc <id>	mark a task incomplete (specify last 5+ characters)
+del <#>	delete a task (specify last 5+ characters)
+com <#>	mark a task complete (specify last 5+ characters)
+inc <#>	mark a task incomplete (specify last 5+ characters)
 quit|q		exit the application
 
 `)
+}
+
+func displayTasks(taskList *tasks.TaskList) {
+	fmt.Printf("%5s  %10s %s %s\n", "Index", "Status", "UUID", "Description")
+
+	for i, task := range *taskList {
+		status := "Complete"
+
+		if task.Completed == false {
+			status = "Incomplete"
+		}
+
+		fmt.Printf("%5d  %10s %s %s\n", i, status, task.Id, task.Description)
+	}
 }
